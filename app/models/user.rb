65bc070a1d06491ca69e98b192801d2e5957842d
aLeX-c-m <alexacatherine.marshall@gmail.com>
2019-07-22 15:31:54 -0400

    creating user model

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+    validates: :name, presence: true, length: { maximum: 10}, uniqueness: {case_sensitive: false}
+
+    has_many: :comments,
+    foreign_key: "user_id",
+    class_name: "Comment"
+
+    has_many: :likes_given,
+    foreign_key: "liker_id",
+    class_name: "Like"
+
+    has_many: :likes_received,
+    foreign_key: "liked_user_id",
+    class_name: "Like"
+
+end
