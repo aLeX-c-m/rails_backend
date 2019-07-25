82c473ed66284c045e3cc60eb54be9e1da0d94ca
aLeX-c-m <alexacatherine.marshall@gmail.com>
2019-07-25 13:45:53 -0400

    seeding with polymorphic associations

--- db/migrate/20190725170959_add_likeable_id_column.rb
+++ db/migrate/20190725170959_add_likeable_id_column.rb
@@ -1,5 +1,5 @@
 class AddLikeableIdColumn < ActiveRecord::Migration[5.2]
   def change
-    add_column :likes, :likeable_id, :integer
+    add_column :likes, :likeable_id, :integer, polymorphic: true, index: true
   end
 end
--- db/migrate/20190725171140_add_likeable_type_column.rb
+++ db/migrate/20190725171140_add_likeable_type_column.rb
@@ -1,5 +1,5 @@
 class AddLikeableTypeColumn < ActiveRecord::Migration[5.2]
   def change
-    add_column :likes, :likeable_type, :string
+    add_column :likes, :likeable_type, :string, polymorphic: true, index: true
   end
 end
