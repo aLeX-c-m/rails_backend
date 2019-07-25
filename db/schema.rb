9b0a536ba0c81b2ff8846d4d3f30c7c6c2416363
aLeX-c-m <alexacatherine.marshall@gmail.com>
2019-07-25 13:12:25 -0400

    adding likeable_type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_170959) do
+ActiveRecord::Schema.define(version: 2019_07_25_171140) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_07_25_170959) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "likeable_id"
+    t.string "likeable_type"
   end
 
   create_table "users", force: :cascade do |t|
